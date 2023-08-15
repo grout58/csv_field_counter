153c133a4edde3d76effaa96d08a44221f1a8617
Jason Grout <jason@grout.dev>
2023-08-15 07:25:53 -0400

    Added time counter.

--- csv_field_counter.py
+++ csv_field_counter.py
@@ -1,8 +1,10 @@
-import csv
+import csv, time
 
 row_list = []
 
 column = int(input('Which column? '))
+
+start_time = time.time()
 
 with open('event.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -12,5 +14,10 @@ with open('event.csv', newline='') as csvfile:
 
 largest_field = max(row_list, key=len)
 largest_field_size =len(max(row_list, key=len))
+
+finish_time = time.time()
+
+total_time = finish_time - start_time
+print(f'Processing took {round(total_time, 2)} seconds to complete.')
 
 print(f'The largest field in column {column} is "{largest_field}" and the size is {largest_field_size} characters long.')
